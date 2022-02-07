bd130c35704e486e9ce261df7554fafdf79fff91
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-02-07 08:05:35 +0100

    Add files via upload

---
+++ com/cc/java/App.java
@@ -0,0 +1,28 @@
+package com.cc.java;
+public class App {
+
+    // static Honeybee worker;
+
+    public static void main(String[] args) {
+      
+        HoneyBee queen = new Queen();
+        HoneyBee drone = new Drone();
+        HoneyBee worker = new Worker();
+
+        output(queen.doYourJob());
+        output(drone.doYourJob());
+        output(worker.doYourJob());
+
+        output("-----------------");
+
+        output(queen.fly());
+        output(drone.fly());
+        output(worker.fly());
+
+  
+    }
+
+    private static void output(String outputStr) {
+        System.out.println(outputStr);
+    }
+}
---
+++ com/cc/java/Worker.java
@@ -0,0 +1,13 @@
+package com.cc.java;
+
+public class Worker extends HoneyBee{
+
+    public String doYourJob() {
+        return "I'm a worker bee, I work all day!";
+     }
+ 
+     @Override
+     public String fly() {
+         return "I believe, I can fly  at 12mph!";
+     }
+}
